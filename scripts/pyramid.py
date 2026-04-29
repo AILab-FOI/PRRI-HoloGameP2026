deedd83550f619c9f68ddbb92955af1250f054a3
flesjak46902 <flesjak21@student.foi.hr>
2026-04-29 11:46:25 +0200

    Create external pyramid script
    Create external pyramid script

---
+++ scripts/pyramid.py
@@ -0,0 +1,35 @@
+from bge import logic, render
+
+scene = logic.getCurrentScene()
+camList = scene.cameras
+cont = logic.getCurrentController()
+own = cont.owner
+
+# Main cameras
+camn = camList[own['camn']]
+cams = camList[own['cams']]
+camw = camList[own['camw']]
+came = camList[own['came']]
+
+a = render.getWindowWidth()
+b = render.getWindowHeight()
+
+x = int((a - b) / 2)
+y = int(b / 4)
+z = int(b / 2)
+
+# Set viewports
+camn.setViewport(x + y, y + z, x + y + z, b)
+cams.setViewport(x + y, 0, x + y + z, y)
+camw.setViewport(x + y + z, y, x + 2 * y + z, y + z)
+came.setViewport(x, y, x + y, y + z)
+#"""
+
+# Enable all viewports
+for cam in [camn, cams, camw, came]:
+    cam.useViewport = True
+
+camn.useViewport = True
+cams.useViewport = True
+camw.useViewport = True
+came.useViewport = True
