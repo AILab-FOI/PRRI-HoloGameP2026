703106397d47d5ce65cd48eed7c531d67e213cb1
jzoricic1 <jzoricic22@student.foi.hr>
2026-05-19 14:49:10 +0200

    Add gear pickup sound and set Level_basement_sound as BGM

--- scripts/player_pickup.py
+++ scripts/player_pickup.py
@@ -1,10 +1,21 @@
 import bge
+import aud
+import os
+
+# Build path: from .blend location, go up, into assets/sounds/
+blend_path = bge.logic.expandPath("//")
+audio_path = os.path.join(blend_path, "..", "sounds", "gear_pickup.mp3")
+audio_path = os.path.normpath(audio_path)
+
+device = aud.Device()
+gear_sound = aud.Sound(audio_path)
 
 def checkPickup(cont):
     own = cont.owner
     sensor = cont.sensors["Collision"]
-
     if sensor.positive:
         for obj in sensor.hitObjectList:
             if "Cog" in obj:
+                handle = device.play(gear_sound)
+                handle.volume = 0.8
                 obj.endObject()
